789c58bd4fcd2b21cfecd76d5573f6cec721e950
Huang-Tsu <huang0966714@gmail.com>
2021-11-30 14:28:42 +0800

    add data section

--- hw4/hw4_test.s
+++ hw4/hw4_test.s
@@ -1,6 +1,41 @@
+/* ========================= */
+/*       DATA section        */
+/* ========================= */
+	.data
+	.align 4
+
+/* --- variable a --- */
+	.type A, %object
+	.size A, 32	//save 32 bytes for matrix A
+A:
+	.word 1 //[1,1]
+	.word 2 //[1,2]
+	.word 3 
+	.word 4 //[1,4]
+	.word 5 //[2,1]
+	.word 6
+	.word 7
+	.word 8 //[2,4]
+
+/* --- variable b --- */
+	.type B, %object
+	.size B, 32 //save 32 bytes for matrix B
+B:
+	.word 8 //[1,1]
+	.word 7 //[1,2]
+	.word 7 //[2,1]
+	.word 6 //[2,2]
+	.word 4
+	.word 3
+	.word 2 //[4,1]
+	.word 1 //[4,2]
+/* ========================= */
+/*       TEXT section        */
+/* ========================= */
 	.section .text
 	.global main
 	.type main,%function
+
 main:
 	MOV ip, sp
 	STDFD sp!,{fp, ip, lr, pc}
