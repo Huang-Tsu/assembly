9ceec6e5623e4db58424e852aae7206605414200
Huang-Tsu <huang0966714@gmail.com>
2022-01-13 01:48:35 +0800

    First commit of hw7 finish generate test data complete C language part left C with SIMD part to be completed

---
+++ hw7/make_rand_data.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GOLDEN_RATIO  0.61803398		//Golden ratio, for hash function
+
+int main(){
+	FILE *fptr = fopen("data.txt", "w+");
+	int i, j;
+	float test;
+	int data;
+	
+	for(i=0; i<400; i++){
+		for(j=0; j<198; j++){
+			data = random();	
+			test = (float)(11.0*((double)data*GOLDEN_RATIO - (int)((double)data*GOLDEN_RATIO)));
+			//fprintf(fptr, "%ld ", random() & ((1<<4)-1));
+			fprintf(fptr, "%f ", test);
+		}
+		fprintf(fptr, "\n");
+	}
+	
+	fclose(fptr);
+	
+	return 0;
+}
